17b7fa32df911e6f44a7b1f512e24b4dd5d3ce4c
osrlazlo <zxp1254@proton.me>
2025-11-02 19:14:23 -0500

    added CI for events routes

---
+++ campusx-app/frontend/src/components/pages/AboutPage.jsx
@@ -0,0 +1,61 @@
+import Footer from "../footer/Footer"
+import Sidebar from "../sidebar/Sidebar"
+import HeroBanner from "./home-page/HeroBanner" 
+
+export default function AboutPage() {
+
+    return(
+    <div className="page-container">
+        <Sidebar/>
+        
+        <div className="home-main-content">
+    
+                <HeroBanner/>
+
+            <div className="content-paragraphs">
+                <p>Campusx is your ultimate hub for discovering and tracking all the exciting happenings on campus. 
+                    Whether you're looking for academic seminars, cultural festivals, career fairs, or social gatherings, we've got you covered. 
+                    Our platform makes it easy to stay connected with the vibrant campus community and never miss out on opportunities that matter to you.</p>
+                <table className="org-members">
+                    <tbody>
+                    <tr><td colSpan={3} className="table-head">Dev Team</td></tr>
+                    <tr><td>Saeed Ramez Fakhouri</td> 
+                        <td>40308146</td> 
+                        <td>SF30-fa</td>
+                        </tr>
+                    <tr><td>Abdoul Kone</td> 
+                        <td>40307505</td> 
+                        <td>osrlazlo</td>
+                        </tr>
+                    <tr><td>Ryan Abdalla</td> 
+                        <td>40243555</td> 
+                        <td>rayanabdalla</td>
+                        </tr>
+                    <tr><td>Abderraouf El Khalil Karoun</td> 
+                        <td>40315753</td> 
+                        <td>Khalil-Krn</td>
+                        </tr>
+                    <tr><td>Madona Beshara</td> 
+                        <td>4031664</td> 
+                        <td>madonabeshara5-ui</td>
+                        </tr>
+                    <tr><td>Mbog Wendy</td> 
+                        <td>40259455</td> 
+                        <td>wenm983</td>
+                        </tr>
+                    <tr><td>Etienne Vorms</td> 
+                        <td>40286823</td> 
+                        <td>Tinou-code</td>
+                        </tr>
+                    <tr><td>Ibrahim Ahmad</td> 
+                        <td>40279224</td> 
+                        <td>ibrahimrocks</td>
+                        </tr>
+                        </tbody>
+                </table>
+            </div>
+            <Footer/>
+        </div>
+    </div>
+    )
+}
